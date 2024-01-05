196ea63cf9a53f25282293c77a3eb6a5f387416d
JoyNgaru <joywanjikum@gmail.com>
2024-01-05 03:09:33 +0000

    debug npx issue, glad it worked coz was damn tired

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,12 @@
 export default function getFullResponseFromAPI(success) {
-    return new Promise((res, reject) => {
-      if (success) {
-        res({
-          status: 200,
-          body: 'Success',
-        });
-      } else {
-        reject(Error('The fake API is not working currently'));
-      }
-    });
-  }
+  return new Promise((res, reject) => {
+    if (success) {
+      res({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
+}
